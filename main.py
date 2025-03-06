a825dae733da65e2f02dcccf1e5a06db33d22651
Jaffa015 <jaffa015@gmail.com>
2025-03-06 12:02:38 +0100

    basic game setup done

---
+++ main.py
@@ -0,0 +1,22 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.display.flip()
+    
+
+if __name__=="__main__":
+    main()
